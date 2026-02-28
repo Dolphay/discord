885d7a13441c3f04ad0990eafb5256f59804d64d
Dolphay1 <ryan.thornton649@gmail.com>
2026-02-27 23:07:59 -0500

    Testing.

--- formatter.go
+++ formatter.go
@@ -265,7 +265,7 @@ func (portal *Portal) parseMatrixHTML(content *event.MessageEventContent, allowe
 		ctx := format.NewContext()
 		ctx.ReturnData[formatterContextInputAllowedLinkPreviewsKey] = allowedLinkPreviews
 		ctx.ReturnData[formatterContextPortalKey] = portal
-		fmt.Printf("%v", portal.Parent.bridge.DMA)
+		fmt.Printf("%v", portal)
 		ctx.ReturnData[formatterContextAllowedMentionsKey] = allowedMentions
 		if content.Mentions != nil {
 			ctx.ReturnData[formatterContextInputAllowedMentionsKey] = content.Mentions.UserIDs
